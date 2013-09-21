e9063f7ec5e102846e563426e25c6dd0ed463e30
k3rb3ros <k3rb3ros@k3rb3ros.com>
2013-09-20 21:59:18 -0600

    Added encode table to Trie class

--- src/huffman_trie.cpp
+++ src/huffman_trie.cpp
@@ -2,9 +2,16 @@
 * By Jesse Blagg and Ryan Morris
 * Huffman Compression program
 */
-
+#include <limits.h>
 #include <iostream>
 using namespace std;
+
+struct Enc_node
+{
+	unsigned long int encoding;
+	int length;
+	Enc_node* buckets;
+};
 
 struct Trie_node
 {
@@ -19,16 +26,18 @@ class Trie
 {
 	private:
 
+	Enc_node enc_table[CHAR_MAX];
 	Trie_node* root;
 	unsigned long int char_count;
 	unsigned long int node_count;
 	void char_traverse(Trie_node* root, const char lookup);
 	void count_traverse(Trie_node* root);
+	void delete_buckets(Enc_node* bkt);
 	void delete_trie(Trie_node* root);
 	void node_traverse(Trie_node* root);
-
+	
 	public:
-
+	
 	Trie();
 	unsigned long int character_count();
 	unsigned long int get_encoding(const char Character);
@@ -48,6 +57,14 @@ void Trie::count_traverse(Trie_node* Root) //Recursively count the number of cha
 	if(Root -> right != NULL) count_traverse(Root -> right);
 	if(Root -> left != NULL) count_traverse(Root -> left);
 	char_count += Root -> val; 
+}
+
+void Trie::delete_buckets(Enc_node* bkt)
+{
+	if(bkt == NULL) return;
+	delete_buckets(bkt -> buckets);
+	bkt -> buckets = NULL;
+	delete bkt; 
 }
 
 void Trie::delete_trie(Trie_node* Root) //Recursively delete Trie data structure
@@ -72,6 +89,22 @@ void Trie::node_traverse(Trie_node* Root) //Recursively count the nodes existing
 	node_count ++;
 }
 
+Trie::Trie() //Constructor inits root node
+{
+	char_count = 0;
+	node_count = 0;
+	root = new Trie_node;
+	root -> is_character = false;
+	root -> val = 0;
+	root -> left = NULL;
+	root -> right = NULL;
+	for(int i=0; i<CHAR_MAX; i++)
+	{
+		enc_table[i].encoding = 0;
+		enc_table[i].length = 0;
+		enc_table[i].buckets = NULL; 
+	}
+}
 unsigned long int Trie::character_count() //Recursively count the number of characters represented by the Trie **FOR DEBUGGING**
 {
 	count_traverse(root);
@@ -82,7 +115,7 @@ unsigned long int Trie::get_encoding(const char Character) //Recursively travers
 {	
 	unsigned long int encode_buff = 0;
 	unsigned long int* enc = &encode_buff;
-
+		
 	return 0;
 }
 
@@ -92,27 +125,20 @@ unsigned long int Trie::size_of_trie() //Recursively count the number of nodes i
 	return node_count;
 }
 
-Trie::Trie() //Constructor inits root node
-{
-	char_count = 0;
-	node_count = 0;
-	root = new Trie_node;
-	root -> is_character = false;
-	root -> val = 0;
-	root -> left = NULL;
-	root -> right = NULL;
-}
-
 Trie::~Trie() //Destructor deletes entire trie
 {
 	delete_trie(root);
+	for(int i=0; i<CHAR_MAX; i++) //Recursively delete any buckets that were allocated on the heap
+	{
+		delete_buckets(enc_table[i].buckets);
+	}
 }
 
-/*int main(int argc, char** argv)
+int main(int argc, char** argv)
 {
 	Trie test;
 	cout << "Constructed and inited\n";
 	cout << "Nodes: " << test.size_of_trie() << endl;
 	cout << "Characters: " << test.character_count() << endl;
 	return 0;
-} */
+}
