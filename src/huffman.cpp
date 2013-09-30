eb5a6e9b29d35df3cb307e7a9a06cf2dd0d920fb
DarkJesterCO <jesseblagg@yahoo.com>
2013-09-29 18:21:45 -0600

    Trouble with write function...

--- src/huffman.cpp
+++ src/huffman.cpp
@@ -30,35 +30,55 @@ void Huffman::compress() //compress the original message
 int Huffman::readHeader(string hfile)
 {
 	int num;
+	int i=0;
+	int f_len = hfile.size();
+	cout << endl << endl << "  charmax = " << CHAR_MAX << endl << endl;
 	string m_number;
 	string f_name;
+	string table;
+	string e_o_f;
 	ifstream inf(hfile.c_str());
-	if(!inf) cout << "Error reading file." << endl;
-	else getline(inf,m_number);
-    
-	//sstream conversion found here: http://www.cplusplus.com/forum/articles/9645/
-	stringstream convert(m_number); // stringstream used for the conversion initialized with the contents of the string
-	if ( !(convert >> num) )//give the value to an integer using the characters in the string
-		num = -1;//if that fails set integer value to 0
-		
-	cout << "m_number as string: " << m_number << "; and converted to an integer num: " << num << endl;
-	if(num != MAGIC_NUMBER)
+	if(!inf) cout << "Error reading file..." << endl;
+	else 
 	{
-		cout << "Not a valid compressed file. Exiting..." << endl;
-		return -1;
+		getline(inf,m_number);
+    
+		//sstream conversion found here: http://www.cplusplus.com/forum/articles/9645/
+		stringstream convert(m_number); // stringstream used for the conversion initialized with the contents of the string
+		if ( !(convert >> num) )//give the value to an integer using the characters in the string
+		{
+			cout << "Not a valid compressed file. Exiting..." << endl;
+			return -1;
+		}
+		else cout << " Magic number matches!" << endl;		
+		getline(inf,f_name);
+		cout << "Original file: " << f_name << endl;
+		//getline(inf,table);	
+		//cout << " Table: " << endl << table;
+		for (int i=0;i<CHAR_MAX;i++) // trouble reading in table with getline and double delimiter ... 762 retrieves complete table.
+		{			
+			inf >> table[i];
+			if(table[i]=='ª' && table[i+1] == '«')
+			{
+				i+=2;
+				cout << ' ';
+			}
+			cout << table[i];
+			i++;
+		} 
+		getline(inf, e_o_f);
+		cout  << endl  << "eof: " << e_o_f; // not working...
 	}
-	else cout << " magic number matches!" << endl;
 	return 0;
 }
 
 void Huffman::populateHeader(string hfile,string fname) // Write the header
 {
 	ofstream outf(hfile.c_str());
-	cout << "Object from other class: " << charTable[45].character << endl;
 	outf << MAGIC_NUMBER << endl << fname << endl;
 	for(int i=0; i<CHAR_MAX; i++) if(charTable[i].active) outf <<  charTable[i].character << DELIM << charTable[i].occurrence;
 	outf << DELIM << DELIM << endl;
-	//for(int i=0;i<CHAR_MAX;i++) outf << huffman_buffer[i];
+	//for(int i=0;i<CHAR_MAX;i++) outf << huffman_buffer[i]; // DON'T DELETE!!
     outf << endl << O_EOF;
 	outf.close();
 }
